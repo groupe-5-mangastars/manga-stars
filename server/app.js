e697634599218cca9419187a92fdaa176991394f
OmarAbdallaa <omar.abdallaa@gmail.com>
2019-07-12 23:02:19 +0200

    login-register-nav-footer
    Signed-off-by: OmarAbdallaa <omar.abdallaa@gmail.com>

--- server/app.js
+++ server/app.js
@@ -1,28 +1,24 @@
-const db = require("./lib/db");
-const express = require("express");
-const MangaRouter = require("./routes/manga");
-const bodyparser = require("body-parser");
+const db = require('./lib/db');
+const User = require('./models/user');
+
+const express = require('express');
+const mangaRouter = require('./routes/manga');
+const userRouter = require('./routes/user');
+const securityRouter = require('./routes/security');
+const verifyToken = require('./middlewares/security');
+const bodyparser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
+
 app.use(bodyparser.json());
-app.use('/manga',MangaRouter);
+app.use('/', securityRouter);
+app.use(verifyToken);
+app.use('/manga', mangaRouter);
+app.use('/users', userRouter);
 
-
-
-app.listen(3000, () => console.log("Listening"));
-
-// db.once('open', () =>
-// {
-//     let user = new User({
-//         email:"lol@hotmail.fr",
-//         password:"salut"
-//         });
-
-//     // user.register().then(data => console.log(data)).catch(error => console.log(erreur));
-//          User.login(user.email,user.password).then(user => console.log(user)).catch(error => console.log(error));
-
-
-// });
-
-// db.on('error', (error) => console.log(error));
-
+app.listen(3000, () => {
+    console.log('Listening');
+});
